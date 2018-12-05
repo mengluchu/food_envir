08c0aadd7a914fa04ecc7f69aef66a882b708ced
Lu <m.lu@uu.nl>
2018-12-05 17:30:31 +0100

    package for the project food environment with maartje

---
+++ R/S1.R
@@ -0,0 +1,10 @@
+#' healthiness score1
+#' @param df_res the dataframe of the resturant environment(count within a buffer)
+#' @param  score the healthiniess score
+#' @details calculated as sum(score * foodenv_count)
+#' @export
+S1 = function(df_res,  score  )
+{
+  mul = sweep(df_res,2, score, "*")
+  apply(mul,1,sum)
+}
---
+++ R/renaming.R
@@ -0,0 +1,22 @@
+#' Replace names of a dataset
+#'  @param dataset the dataset
+#' @param name2replace greplstyle name to replace, e.g. "loc"
+#' @param replacenames the names to replace to, a list match the length of names to be replaced
+#' @export
+renaming = function(dataset, names2replace="loc", replacenames)
+{
+  namesd2 =names(dataset)[which(grepl(names2replace, names(dataset)))]
+  rp = rep(sapply(replacenames, function(x) substr(x, start = 15, stop =nchar(x)-4)), each = 4)
+  trp =  rep(sapply(replacenames, function(x) substr(x, start = 1, stop =14)),each = 4)
+
+  fn = 0
+  le = length(namesd2)
+  for ( i in 1: le)
+  {
+    fn = rbind(fn, gsub(trp[i], rp[i],namesd2[i] ))
+  }
+  fn = fn[-1]
+
+  names(dataset)[which(grepl(names2replace, names(dataset)))] = fn
+  return(dataset)
+}
